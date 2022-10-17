553b3f5cff1a1f748c9ddc4d5f567cadc85c1ec4
Kyuwoong Lee <kwooler@gmail.com>
2022-10-16 20:45:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-def 	getinput():
 
-	# ******************************
-	# Make your Code
-	# ******************************
+def getinput(n1, n2):
+# 	print(id(n1))
+	n1 = input('Enter a number')
+	n2 = input('Enter a number')
 
-def 	getsum(v1, v2):
-	# ******************************
-	# Make your Code
-	# ******************************
-
-def	printval(v1, v2, total):
-	# ******************************
-	# Make your Code
-	# ******************************
-
-def 	main():
-	# ******************************
-	# Make your Code
-	# ******************************
-
+def main():
+	num1 = num2 = 0 
+	getinput(num1, num2)
+	print (num1, num2)
 
 if __name__ == '__main__':
 	main()
-	
-
